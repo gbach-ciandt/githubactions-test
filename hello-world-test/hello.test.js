d91e0dcd300573962342c17298dcd4c5c4ac5922
Gabriel Cassol Bach <gabrielcassolbach@gmail.com>
2025-07-28 10:34:08 -0300

    bazuca

--- hello-world-test/hello.test.js
+++ hello-world-test/hello.test.js
@@ -1,3 +1,14 @@
 test('Hello World test', () => {
     expect('Hello World').toBe('Hello World');
 });
+
+function countTo100() {
+    return Array.from({ length: 100 }, (_, i) => i + 1);
+}
+
+describe('Count to 100', () => {
+    test('should generate an array counting from 1 to 100', () => {
+        const result = countTo100();
+        expect(result).toEqual(Array.from({ length: 100 }, (_, i) => i + 1));
+    });
+});
